312021aff68f55e3b08121dfa7d60d15941a1c83
vivalalova <vivalalova0@gmail.com>
2021-09-11 18:03:04 +0800

    Initial Commit

---
+++ PageTab/PageTabApp.swift
@@ -0,0 +1,17 @@
+//
+//  PageTabApp.swift
+//  PageTab
+//
+//  Created by Lova on 2021/9/11.
+//
+
+import SwiftUI
+
+@main
+struct PageTabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
