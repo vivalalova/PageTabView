98ce99691ffe2f39eb15081e7885c671165bc0d5
vivalalova <vivalalova0@gmail.com>
2021-12-17 23:56:34 +0800

    add

---
+++ Sources/PageTabView/EnvironmentValues.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Lova on 2021/12/17.
+//
+
+import Combine
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+private struct MyEnvironmentKey: EnvironmentKey {
+    static let defaultValue: (Int) -> Void = { _ in }
+}
+
+@available(iOS 13.0.0, *)
+extension EnvironmentValues {
+    var onPageUpdate: (Int) -> Void {
+        get { self[MyEnvironmentKey.self] }
+        set { self[MyEnvironmentKey.self] = newValue }
+    }
+}
+
+@available(iOS 13.0.0, *)
+public
+extension View {
+    func onPageUpdate(_ onPageUpdate: @escaping (Int) -> Void) -> some View {
+        environment(\.onPageUpdate, onPageUpdate)
+    }
+}
--- Sources/PageTabView/PageTabView.swift
+++ Sources/PageTabView/PageTabView.swift
@@ -8,27 +8,6 @@
 import Combine
 import SwiftUI
 
-@available(iOS 13.0.0, *)
-private struct MyEnvironmentKey: EnvironmentKey {
-    static let defaultValue: (Int) -> Void = { _ in }
-}
-
-@available(iOS 13.0.0, *)
-extension EnvironmentValues {
-    var onPageUpdate: (Int) -> Void {
-        get { self[MyEnvironmentKey.self] }
-        set { self[MyEnvironmentKey.self] = newValue }
-    }
-}
-
-@available(iOS 13.0.0, *)
-public
-extension View {
-    func onPageUpdate(_ onPageUpdate: @escaping (Int) -> Void) -> some View {
-        environment(\.onPageUpdate, onPageUpdate)
-    }
-}
-
 @available(iOS 14.0.0, *)
 public
 struct PageTabView<Content: View>: View {
@@ -37,14 +16,14 @@ struct PageTabView<Content: View>: View {
     @Environment(\.onPageUpdate) var onPageUpdate: (Int) -> Void
 
     var titles: [AnyView] = []
-    let content: () -> Content
 
-    public
-    init<V0: View, V1: View>(
+    let content: Content
+
+    public init<V0: View, V1: View>(
         @ViewBuilder titleView: @escaping () -> TupleView<(V0, V1)>,
         @ViewBuilder content: @escaping () -> Content
     ) {
-        self.content = content
+        self.content = content()
         let cv = titleView().value
         self.titles = [AnyView(cv.0), AnyView(cv.1)]
         self.model.onPageUpdate = self.onPageUpdate
@@ -68,19 +47,35 @@ struct PageTabView<Content: View>: View {
             VStack(spacing: 0) {
                 setup(frame)
 
-                Head(frame)
+                HeadView(titles: titles, frame: frame)
+                    .environmentObject(model)
 
-                ContentBody(frame)
+                ContentView(titles: titles, frame: frame, content: content)
+                    .environmentObject(model)
             }
         }
     }
+}
 
-    private func Head(_ frame: GeometryProxy) -> some View {
-        func Btn(_ index: Int) -> some View {
-            Button(action: self.model.onPress(index: index, width: frame.size.width)) {
-                self.titles[index]
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+@available(iOS 14.0.0, *)
+extension PageTabView {
+    struct HeadView: View {
+        var titles: [AnyView] = []
+        var frame: GeometryProxy
+        @EnvironmentObject var model: PageTabView.Model
+
+        var body: some View {
+            HStack(spacing: 0) {
+                ForEach(0 ..< self.titles.count) { index in
+                    if index == 0 {
+                        Btn(index)
+                            .overlay(HorizontalBar(), alignment: .bottom)
+                    } else {
+                        Btn(index)
+                    }
+                }
             }
+            .frame(height: 44)
         }
 
         func HorizontalBar() -> some View {
@@ -95,64 +90,62 @@ struct PageTabView<Content: View>: View {
             // Use Bar Width to calculate Button counts
         }
 
-        return HStack(spacing: 0) {
-            ForEach(0 ..< self.titles.count) { index in
-                if index == 0 {
-                    Btn(index)
-                        .overlay(HorizontalBar(), alignment: .bottom)
-                } else {
-                    Btn(index)
-                }
+        func Btn(_ index: Int) -> some View {
+            Button(action: self.model.onPress(index: index, width: self.frame.size.width)) {
+                self.titles[index]
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
-        .frame(height: 44)
     }
 
-    private func ContentBody(_ proxy: GeometryProxy) -> some View {
-        PageScrollView(numberOfPage: self.titles.count, offset: self.$model.offset) {
-            self.content()
-                // Subscribe ScrollView ContentOffset
-                .overlay(
-                    GeometryReader { offsetProxy in
-                        Color.clear
-                            .preference(key: TabPreferenceKey.self, value: offsetProxy.frame(in: .global))
+    struct ContentView<Content: View>: View {
+        var titles: [AnyView] = []
+        var frame: GeometryProxy
+        @EnvironmentObject var model: PageTabView.Model
+
+        var content: Content
+
+        var body: some View {
+            PageScrollView(numberOfPage: self.titles.count, offset: self.$model.offset) {
+                self.content
+//                    .frame(width: frame.size.width)
+                    // Subscribe ScrollView ContentOffset
+                    .overlay(
+                        GeometryReader { offsetProxy in
+                            Color.clear
+                                .preference(key: TabPreferenceKey.self, value: offsetProxy.frame(in: .global))
+                        }
+                    )
+                    // Then Set Offset
+                    .onPreferenceChange(TabPreferenceKey.self) { offsetProxy in
+                        self.model.barOffset = -offsetProxy.minX / CGFloat(titles.count)
                     }
-                )
-                // Then Set Offset
-                .onPreferenceChange(TabPreferenceKey.self) { offsetProxy in
-                    self.model.barOffset = -offsetProxy.minX / CGFloat(titles.count)
-                }
+            }
         }
     }
 }
 
 @available(iOS 14.0.0, *)
 struct PageTabView_Previews: PreviewProvider {
-    struct ExtractedView: View {
-        var body: some View {
-            PageTabView {
-                Text("red").foregroundColor(.red)
-                Text("green").foregroundColor(.green)
-            } content: {
-                VStack {
-                    Button("red") {
-//                        model.scrollTo(page: 1)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-
-                VStack {
-                    Button("green") {
-//                        model.scrollTo(page: 0)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .accentColor(.black)
-        }
-    }
-
     static var previews: some View {
-        ExtractedView()
+        PageTabView {
+            Text("red").foregroundColor(.red)
+            Text("green").foregroundColor(.green)
+        } content: {
+            VStack {
+                Button("red") {
+//                    scrollTo(1)
+                }
+            }
+            .frame(maxWidth: .infinity)
+
+            VStack {
+                Button("green") {
+//                    scrollTo(0)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .accentColor(.black)
     }
 }
