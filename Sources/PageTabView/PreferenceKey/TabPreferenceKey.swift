97d7df67081c987ca765ccb1e59e780f2bea6700
vivalalova <vivalalova0@gmail.com>
2022-11-22 02:48:04 +0800

    tmp

--- Sources/PageTabView/PreferenceKey/TabPreferenceKey.swift
+++ Sources/PageTabView/PreferenceKey/TabPreferenceKey.swift
@@ -16,3 +16,44 @@ struct TabPreferenceKey: PreferenceKey {
         value = nextValue()
     }
 }
+
+public
+struct PageTitlePreferenceKey: PreferenceKey {
+    public typealias T = any View
+
+    public static var defaultValue: T = EmptyView()
+
+    public static func reduce(value: inout T, nextValue: () -> T) {
+        value = nextValue()
+    }
+}
+
+// MARK: - TitleView
+
+struct TitleViewPreferenceKey: PreferenceKey {
+    static var defaultValue = Wrapper(value: EmptyView())
+
+    static func reduce(value: inout Wrapper, nextValue: () -> Wrapper) {
+        value = nextValue()
+    }
+
+    struct Wrapper: Equatable {
+        var value: any View = EmptyView()
+
+        static func == (lhs: Wrapper, rhs: Wrapper) -> Bool {
+            true
+        }
+    }
+}
+
+extension View {
+    var wrapped: TitleViewPreferenceKey.Wrapper {
+        TitleViewPreferenceKey.Wrapper(value: self)
+    }
+}
+
+extension View {
+    func pageTitle(view: any View) -> some View {
+        self.preference(key: TitleViewPreferenceKey.self, value: view.wrapped)
+    }
+}
